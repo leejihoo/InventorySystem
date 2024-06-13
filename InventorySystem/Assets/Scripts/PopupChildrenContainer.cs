157aa989393e4a85d38a5df1a3927499a514198f
leejihoo <ajtwlsdl33@gmail.com>
2024-06-13 17:58:37 +0900

    [생성] Popup 창에 필요한 컴포넌트들에 대한 참조를 저장한 클래스 생성

---
+++ InventorySystem/Assets/Scripts/PopupChildrenContainer.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopupChildrenContainer : MonoBehaviour
+{
+    public Image itemImage;
+    public TMP_Text itemName;
+    public TMP_Text itemType;
+    public TMP_Text itemDescription;
+    public TMP_Text itemEffect;
+}
