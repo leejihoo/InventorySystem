7791ea56f93c7eefaa1ec75ec49c93b90390a68d
leejihoo <ajtwlsdl33@gmail.com>
2024-06-13 17:57:19 +0900

    [기능 추가] 이미지 클릭 시 아이템 정보 팝업 기능 추가

--- InventorySystem/Assets/Scripts/ItemContainer.cs
+++ InventorySystem/Assets/Scripts/ItemContainer.cs
@@ -1,8 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class ItemContainer: MonoBehaviour
+public class ItemContainer: MonoBehaviour, IPointerClickHandler
 {
     public BaseItem BaseItem;
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (BaseItem.BaseItemModel.Type == ItemType.Equipment)
+        {
+            PopupManager.Instance.CreateEquipmentItemPopup(BaseItem);
+        }
+    }
 }
