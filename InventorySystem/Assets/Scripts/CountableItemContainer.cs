0a49e7a0badac89f4542d035214b3e7c2d315b2a
leejihoo <ajtwlsdl33@gmail.com>
2024-07-01 18:16:02 +0900

    [스크립트 추가] ItemContainer를 상속받는 클래스 생성
    아이템 갯수 텍스트를 캐싱하기 위한 클래스 생성, 기존 코드에 텍스트 변수 추가 시 장비 아이템에서는 사용하지 않는 변수가 되어 메모리를 낭비하게 되기 때문에 새로운 클래스를 생성

---
+++ InventorySystem/Assets/Scripts/CountableItemContainer.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CountableItemContainer : ItemContainer
+{
+    public TMP_Text itemCount;
+}
